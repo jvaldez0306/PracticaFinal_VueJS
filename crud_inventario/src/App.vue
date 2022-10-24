f5ba7addf9720777b8fc61bf00207635f85f930b
Jose Valdez <jose.avaldezz0306@gmail.com>
2022-10-24 01:07:50 -0400

    updated

--- crud_inventario/src/App.vue
+++ crud_inventario/src/App.vue
@@ -1,4 +1,6 @@
 <template>
+<div class="container">
+
   <h1>Inventario de Activos</h1>
   <div class="row">
     <div class="col m12 card-panel">
@@ -12,44 +14,56 @@
       </div>
     </div>
   </div>
-
-<div class="row">
-  <div class="col m3">
-    <label>Modelo</label>
-    <input type="text" v-model="modelo">
-  </div>
+  
+  <div class="row">
+    <div class="col m3">
+      <label>Modelo</label>
+      <input type="text" v-model="modelo">
+    </div>
   <div class="col m4">
-    <label>Estado</label>
+    <label>Estado Activo</label>
     <select v-model="estado_activo">
       <option value="N">Nuevo</option>
       <option value="U">Usado</option>
       <option value="D">Desuso</option>
-
     </select>
   </div>
 </div>
- 
+<div class="col m4">
+  <label>Area Relacionada</label>
+  <input type="text" v-model="arearel">
+</div>
+<div class="row">
+  <button type="submit" class="btn indigo darken-4">AGREGAR ACTIVO<i class="material-icons">add.circle</i></button>
+</div>
+
+</div>
 </template>
 
 <script>
 import M from 'materialize-css'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      tipo:'',
+      marca:'',
+      modelo:'',
+      estado:'',
+      estados:[],
+      arearel:'',
+
+      select_instances: []
+
+    }
+  },
+
   mounted()
   {
-    this.axios.get('https://prueba.com')
-    .then(() =>{
-
-    });
-  },
-  methods:{
-    saludar()
-    {
-      M.toast({html: 'Hola Mundo'});
-    }
+    var elems = document.querySelectorAll('select');
+    this.select_instances = M.FormSelect.init(elems,null);
   }
-
+  
   }
 
 </script>
